2aa2886d15ea59734dbc642370857b2fd8c22120
fabianmoronzirfas <fabian.moron.zirfas@gmail.com>
2018-12-18 14:21:17 +0100

    fix(reference index): Fixing the index and cleaning some unused things
    Fixied also lag in loading due to wrapping the main into an element using jquery. All things that jQ
    did for all pages are now part ofthe markup
    
    close #60 re #79

--- assets/js/basiljs.js
+++ assets/js/basiljs.js
@@ -1,103 +1,43 @@
-/* globals jQuery */
-/* Author:
-	Philipp C. Adrian
-*/
-(function($, window, undefined) {
-////////////////////////////////////////////////////////////////////////////////
-  const bjs = {
-    ////////////////////////////////////////////////////////////////////////////////
-    init : function (){
-      //$("html").removeClass("no-js");
-
-      //bjs.activateReferenceItem(location.hash);
-      $('pre.code').highlight({source:1, zebra:1, indent:'space', list:'ol'});
-
-      //bjs.behaviour($("body"));
-    },
-    ////////////////////////////////////////////////////////////////////////////////
-    behaviour: function (context) {
-      $('.referenceLink', context).bind('click',function(e){
-        e.preventDefault();
-        bjs.activateReferenceItem($(this).attr('href'));
-      });
-
-      bjs.iScroll = new iScroll('iScroll', {
-        hideScrollbar:false,
-        hScroll:false
-      });
-    },
-    ////////////////////////////////////////////////////////////////////////////////
-    iScroll : false,
-    activateReferenceItem : function(id) {
-      var content = $(id);
-      if (!content.length) return;
-      window.location=id;
-      $('.referenceLink').removeClass('active');
-      $('[href="'+id+'"]').addClass('active');
-      content.siblings().removeClass('.active').fadeOut(100,function(){
-        content.addClass('.active').css({display:'none',opacity:1}).fadeIn(100);
-      });
-    }
-  };
-  ////////////////////////////////////////////////////////////////////////////////
-  $(document).ready(function() {
-    bjs.init();
-    $( 'body > *' ).wrapAll( '<div class=\'wrapper\'></div>' );
-    specialChars();
-    createGuideLines();
-
-    $('.nav-o-meter').append('<ul class="bars"><li></li><li></li><li></li></ul>');
-    // MOBILE NAVIGATION
-    $('.nav-o-meter').on('click', function() {
-      $(this).toggleClass('opened');
-      $('	.main-menu').toggle();
-
-      $('.main-menu').find('li a').bind('click', function(){
-        if ($('.nav-o-meter').hasClass('opened')) {
-          $('.nav-o-meter').toggleClass('opened');
-          $('	.main_menu').toggle();
-        }
-      });
+$(document).ready(function() {
+  specialChars();
+  createGuideLines();
+  // MOBILE NAVIGATION
+  $('.nav-o-meter').on('click', function() {
+    $(this).toggleClass('opened');
+    $('.main-menu').toggle();
+    $('.main-menu').find('li a').bind('click', function() {
+      if ($('.nav-o-meter').hasClass('opened')) {
+        $('.nav-o-meter').toggleClass('opened');
+        $('.main_menu').toggle();
+      }
     });
   });
+});
 
-
-  function specialChars() {
-    $('a.stepped-link').each( function() {
-      const chars = $(this).text().split('');
-      const that = $(this);
-      that.empty();
-      $.each(chars, function (i, char) {
-        const fontSize = 30 - (2*i);
-        const style = `border-bottom-width: ${ (i+1) }px; font-size: ${fontSize }px; top: -${(i+(2*i))}px;`;
-
-        // var style = 'style="border-bottom-width: ' + (i+1) + 'px; ';
-        // style += 'font-size: '+fontSize+'px; ';
-        // style += 'top: -' + (i+(2*i)) + 'px;"';
-        that.append('<span style="' +style+ '">' + char + '</span>');
-      });
-      // for(var i = 0; i < chars.length; i++) {
-      //   chars[i] = '<span>'+chars[i]+'</span>';
-      // 	console.log(chars[i]);
-      // }
+function specialChars() {
+  $('a.stepped-link').each(function() {
+    const chars = $(this).text().split('');
+    const that = $(this);
+    that.empty();
+    $.each(chars, function(i, char) {
+      const fontSize = 30 - (2 * i);
+      const style = `border-bottom-width: ${ (i+1) }px; font-size: ${fontSize }px; top: -${(i+(2*i))}px;`;
+      that.append('<span style="' + style + '">' + char + '</span>');
     });
+  });
+}
+
+function createGuideLines() {
+  $('.guidelines').append('<div class="bottom_lines">');
+  for (var b = 0; b < 5; b++) {
+    $('.bottom_lines').append('<div class="bottomLine line_' + (b + 1) + '">');
   }
-
-
-  function createGuideLines() {
-    $('.guidelines').append('<div class="bottom_lines">');
-    for (var b=0; b<5; b++) {
-      $('.bottom_lines').append('<div class="bottomLine line_'+(b+1)+'">');
-    }
-    $('.guidelines').append('<div class="right_lines">');
-    for (var r=0; r<3; r++) {
-      $('.right_lines').append('<div class="right_line line_'+(r+1)+'">');
-    }
-    $('.guidelines').append('<div class="page_lines">');
-    for (var i=0; i<4; i++) {
-      $('.page_lines').append('<div class="line line_'+(i+1)+'">');
-    }
+  $('.guidelines').append('<div class="right_lines">');
+  for (var r = 0; r < 3; r++) {
+    $('.right_lines').append('<div class="right_line line_' + (r + 1) + '">');
   }
-
-////////////////////////////////////////////////////////////////////////////////
-})(jQuery, window, undefined);
+  $('.guidelines').append('<div class="page_lines">');
+  for (var i = 0; i < 4; i++) {
+    $('.page_lines').append('<div class="line line_' + (i + 1) + '">');
+  }
+}
