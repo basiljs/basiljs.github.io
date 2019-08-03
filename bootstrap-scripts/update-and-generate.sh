a77838bb388a252cea4b24a539dd1734af8e8d50
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-08-03 07:35:08 +0200

    docs: Update README and npm-scripts to be up to date

--- bootstrap-scripts/update-and-generate.sh
+++ bootstrap-scripts/update-and-generate.sh
@@ -5,4 +5,4 @@ pwd
 npm install basiljs/basil.js#develop -D
 cp ./node_modules/basiljs/changelog.txt ./_source/download/
 npm run documentationjs -- build ./node_modules/basiljs/basil.js -o ./build-scripts/api/data.json
-node build-scripts/dist/index.js
+node build-scripts/src/index.js
