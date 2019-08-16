e31423a58e225120da6d61f97e98f30ac7072fc6
Fabian Morón Zirfas <icke@fabianmoronzirfas.me>
2019-08-16 11:51:05 +0200

    chore(update deps) eslint and fix buildscript

--- bootstrap-scripts/update-and-generate.sh
+++ bootstrap-scripts/update-and-generate.sh
@@ -2,7 +2,15 @@
 set -Eeuo pipefail
 pwd
 
+echo "Installing latest develop version of Basil.js"
 npm install basiljs/basil.js#develop -D
+echo "Success"
+echo "Copying changelog.txt into jekyll source"
 cp ./node_modules/basiljs/changelog.txt ./_source/download/
+echo "Success"
+echo "Building JSDoc data.json using documentationjs"
 npm run documentationjs -- build ./node_modules/basiljs/basil.js -o ./build-scripts/api/data.json
-node build-scripts/src/index.js
+echo "Success"
+echo "Building markup from data.json"
+node build-scripts/index.js
+echo "Success"
